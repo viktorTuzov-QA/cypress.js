1ef4960722bf6e35184b68c866d97935a2317008
Viktor <111768606+viktorTuzov-QA@users.noreply.github.com>
2022-10-03 20:38:04 +0500

    Add files via upload
    улучшил селекторы

--- form_auth-staya.js
+++ form_auth-staya.js
@@ -1,20 +1,20 @@
 describe('авторизация staya ', function () {
     it('Форма авторизации. Авторизация. Позитивные данные', function () {
          cy.visit('https://staya.dog');
-         cy.get("#stickyHeader > section.header-bottom.transition-header > div > div > a").click();
-         cy.get('.auth-form > form > [type="email"]').type("rs503@mail.ru");
-         cy.get('.auth-form > form > [type="password"]').type("QQqq12345678");
+         cy.get(".header-bottom__right--link[data-v-51321da9]").click();
+         cy.get("[data-v-3fa00444][type='email']").type("rs503@mail.ru");
+         cy.get("[data-v-3fa00444][type='password']").type("QQqq12345678");
          cy.get(".auth-form__submit").click();
          cy.contains('Мои заказы');
-         cy.get("#__layout > div > main > div > div.container > div > aside > button").click();
-         cy.get("#__layout > div > div > section > div > div > div > button.box__button.box__button_ok.s-button.s-button_theme-dark").click();
+         cy.get("[data-v-698c6e54][type='button']").click();
+         cy.get(".box__button.box__button_ok").click();
        
      })
 
      it('Форма авторизации. Авторизация. Неверный пароль', function () {
-        cy.get("#stickyHeader > section.header-bottom.transition-header > div > div > a").click();
-        cy.get('.auth-form > form > [type="email"]').type("rs503@mail.ru");
-        cy.get('.auth-form > form > [type="password"]').type("QQqq12345679");
+        cy.get(".header-bottom__right--link[data-v-51321da9]").click();
+        cy.get("[data-v-3fa00444][type='email']").type("rs503@mail.ru");
+        cy.get("[data-v-3fa00444][type='password']").type("QQqq12345679");
         cy.get(".auth-form__submit").click();
         cy.contains('Невозможно войти с предоставленными учетными данными.');
         
@@ -23,4 +23,5 @@ describe('авторизация staya ', function () {
     })
      
  })
+ 
  
